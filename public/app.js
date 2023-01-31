f40165ac5a38c39ce73f840b76a327d1673f3a74
Sounisa <119640964+sounisa@users.noreply.github.com>
2023-01-31 13:08:50 -0800

    108

--- public/app.js
+++ public/app.js
@@ -1,23 +1,38 @@
-const button = document.getElementById('post-btn');
-
-button.addEventListener('click', async _ => {
-  try {     
-    const response = await fetch('/pokemons', {
-      method: 'post',
-      body: {
-        // Your body
-      }
-    });
-    console.log('Completed!', response);
-  } catch(err) {
-    console.error(`Error: ${err}`);
-  }
-});
-
-async function getData() {
-	const response = await fetch('/pokemons')
-  const data = await response.json() 
-  console.log(data)
-}
 
 getData()
+
+//GET ALL
+async function getData() {
+    const response = await fetch('/pokemons')
+    const data = await response.json() 
+    console.log(data)
+    showAllPokemons(data)
+
+}
+
+
+function showAllPokemons(data) {
+    for (let i = 0; i < data.length; i++) {
+        let pokeCard = document.createElement('span')
+        pokeCard.className = "pokemon-card"
+        pokeCard.id = data[i].name
+        let actualPoke = document.createElement('div')
+        actualPoke.className = "pokemon"
+        let pokeName = document.createElement('h1')
+        pokeName.className = "pokemonName"
+        pokeName.textContent = data[i].name
+        let pokeType = document.createElement('h3')
+        pokeType.className = "pokemonType"
+        pokeType.textContent = data[i].type
+        let pokeHp = document.createElement('h3')
+        pokeHp.className = "pokemonHp"
+        pokeHp.textContent = data[i].hp
+        let pokeContainer = document.getElementsByClassName('all-pokemons-container')
+        actualPoke.appendChild(pokeName)
+        actualPoke.appendChild(pokeType)
+        actualPoke.appendChild(pokeHp)
+        pokeCard.appendChild(actualPoke)
+        pokeContainer.appendChild(pokeCard)
+    }
+}
+
