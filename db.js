60b7956468c9aa17bd8bee6d92cb24f6e58b4e0d
Sounisa <119640964+sounisa@users.noreply.github.com>
2023-01-31 20:08:22 -0800

    notlocal

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const dbConfig = {
-    connectionString: process.env.LOCAL
+    connectionString: process.env.DATABASE_URL
 }
 
 const client = new Pool(dbConfig);
