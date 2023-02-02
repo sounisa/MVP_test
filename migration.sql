50fb10b5fa46b4ac02035739b850e8a0b25aebc4
Sounisa <119640964+sounisa@users.noreply.github.com>
2023-02-01 23:21:09 -0800

    new

--- migration.sql
+++ migration.sql
@@ -4,5 +4,7 @@ CREATE TABLE pokemons(
    id SERIAL PRIMARY KEY NOT NULL,
    name VARCHAR NOT NULL,
    type VARCHAR NOT NULL,
-   hp INT NOT NULL
+   hp INT NOT NULL,
+   color VARCHAR NOT NULL,
+   img TEXT NOT NULL
 );
--- seed.sql
+++ seed.sql
@@ -1,1 +1,1 @@
-INSERT INTO pokemons (name, type, hp) VALUES ('Squirtle', 'water', 20);
+INSERT INTO pokemons (name, type, hp, color, img) VALUES ('Squirtle', 'water', 20, 'blue', '#3393dd');
