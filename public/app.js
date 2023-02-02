6a9866722a5f7d0b460f953fd994ecedbd95ca4f
Sounisa <119640964+sounisa@users.noreply.github.com>
2023-02-02 01:26:48 -0800

    newnew

--- public/app.js
+++ public/app.js
@@ -1,7 +1,20 @@
 const postBtn = document.querySelector("#postbtn")
 let deleteThis = document.querySelectorAll(".x")
-
+console.log(document.querySelectorAll(".x"))
+console.log(document.querySelector("#postbtn"))
 getData()
+
+$('#show').on('click', function () {
+    $('.logo').hide();
+    $('.center').show();
+})
+
+
+$('#close-postbtn').on('click', function () {
+    $('.center').hide();
+    $('.logo').show();
+    $('#show').show();
+})
 
 //GET ALL
 async function getData() {
@@ -9,57 +22,36 @@ async function getData() {
     const data = await response.json() 
     console.log(data)
     showAllPokemons(data)
+    addToData(data)
+    //postData(data)
 }
-
 function showAllPokemons(data) {
     for (let i = 0; i < data.length; i++) {
-
         let pokeCard = document.createElement('span') //PokemonCard 
         pokeCard.className = "pokemon-card"
         pokeCard.id = data[i].name
-
         let pokeName = document.createElement('div')//Pokemon's Name
         pokeName.className = "pokeName"
         pokeName.textContent = data[i].name
-
         let pokeType = document.createElement('div')//Pokemon's Type
         pokeType.className = "pokeType"
-        pokeType.textContent = data[i].type.toUpperCase()
-
+        pokeType.textContent = data[i].type
         let pokeHp = document.createElement('div')//Pokemon's HP
         pokeHp.className = "pokeHp"
         pokeHp.textContent = `${data[i].hp} HP`
-
-        let pokeImg = document.createElement ('img')//Pokemon's Type Img
-        pokeImg.className = 'pokeImg'
-        pokeImg.src = data[i].img
-
         let deleteBtn = document.createElement('button')//delete button
         deleteBtn.className= "x"
         deleteBtn.id = data[i].id
         deleteBtn.textContent = "X"
         addListenerToDeleteButton(deleteBtn)//add listener to each delete button
-
-
         let pokeContainer = document.querySelector('.all-pokemons-container')
         pokeCard.appendChild(deleteBtn)
         pokeCard.appendChild(pokeName)
-        pokeCard.appendChild(pokeImg)
         pokeCard.appendChild(pokeType)
         pokeCard.appendChild(pokeHp)
-
-        let color = data[i].color
-        pokeCard.style.color = `#${data[i].color}` //changes color of font based off pokemon type
         pokeContainer.appendChild(pokeCard)
     }
 }
-
-$('#show').on('click', function () { //add new pokemon button
-    $('.logo').hide();
-    $('.center').show();
-})
-
-
 //add New Pokemon Button, get values, post new pokemon
 postBtn.addEventListener("click", function (e) {
     e.preventDefault();
@@ -69,20 +61,12 @@ postBtn.addEventListener("click", function (e) {
     const newPokeName = document.getElementById('pokemon-name').value
     const newPokeType = document.getElementById('pokemon-type').value
     const newPokeHp = document.getElementById('pokemon-hp').value
-    alert(`${newPokeType} Type Pokemon: ${newPokeName} with ${newPokeHp}HP was added to your Pokedex`)
+    alert(`${newPokeType} Type Pokemon: ${newPokeName} with HP: ${newPokeHp} was added to your Pokedex`)
+    $('#testCard').hide();
     postNewPokemon(newPokeName, newPokeType, newPokeHp)
 });
-
-$('#close-postbtn').on('click', function () { //X on form
-    $('.center').hide();
-    $('.logo').show();
-    $('#show').show();
-})
-
 //POST
-const postNewPokemon = async (newPokeName, newPokeType, newPokeHp) => {
-    let color = assignColor(newPokeType)
-    let logo = assignLogo(newPokeType)
+async function postNewPokemon(newPokeName, newPokeType, newPokeHp) {
     const options = {
         method: 'POST',
         headers: {
@@ -90,17 +74,14 @@ const postNewPokemon = async (newPokeName, newPokeType, newPokeHp) => {
             'content-type': 'application/json',
         },
         body: JSON.stringify({
-            "name": `${newPokeName}`,
-            "type": `${newPokeType}`,
-            "hp": `${newPokeHp}`,
-            "color": `${color}`,//calls assigncolor function
-            "img": `${logo}` //calls assignlogo function
+            "name": newPokeName,
+            "type": newPokeType,
+            "hp": newPokeHp
         })
     }
     const response = await fetch('/pokemons', options)
     const newData = await response.json()
 }
-
 //adds event listener to x attached to that pokemoncard, if clicked, deletes pokemon
 function addListenerToDeleteButton(deleteBtn) {
     deleteBtn.addEventListener("click", function (e) {
@@ -108,61 +89,60 @@ function addListenerToDeleteButton(deleteBtn) {
         deletePokemon(deleteBtn)
     })
 }
-
-
 //DELETE 1
 async function deletePokemon(deleteBtn) {
     const options = {
         method: 'DELETE'
     }
     const response = await fetch(`/pokemons/${deleteBtn.id}`, options)
+    const sqlQuery = await response.json() 
     const dataAfterDelete = await response.json() 
     console.log(dataAfterDelete)
 }
 
 
 
-//assign color to pokemon type
-function assignColor(newPokeType){
-    if (newPokeType.toLowerCase() === "dark") {
-         return '5b5366' //rgba(91,83,102,255)
-    } else if (newPokeType.toLowerCase() === 'water'){
-        return '3393dd' //rgba(51,147,221,255)
-    } else if (newPokeType.toLowerCase() === 'fire') {
-        return 'da2c33' //rgba(218,44,51,255)
-    } else if (newPokeType.toLowerCase() === 'grass'){
-        return '02913a' //rgba(2,145,58,255)
-    } else if (newPokeType.toLowerCase() === 'rock') {
-        return 'c9b787' //rgba(201,183,135,255)
-    } else if (newPokeType.toLowerCase() === 'normal') {
-        return '929ba3' //rgba(146,155,163,255)
-    } else if (newPokeType.toLowerCase() === 'electric') {
-        return 'fbd200' //rgba(251,210,0,255)
-    } else {
-        return 'black'
-    }
+// //assign color to pokemon type
+// function assignColor(newPokeType){
+//     if (newPokeType.toLowerCase() === "dark") {
+//          return '5b5366' //rgba(91,83,102,255)
+//     } else if (newPokeType.toLowerCase() === 'water'){
+//         return '3393dd' //rgba(51,147,221,255)
+//     } else if (newPokeType.toLowerCase() === 'fire') {
+//         return 'da2c33' //rgba(218,44,51,255)
+//     } else if (newPokeType.toLowerCase() === 'grass'){
+//         return '02913a' //rgba(2,145,58,255)
+//     } else if (newPokeType.toLowerCase() === 'rock') {
+//         return 'c9b787' //rgba(201,183,135,255)
+//     } else if (newPokeType.toLowerCase() === 'normal') {
+//         return '929ba3' //rgba(146,155,163,255)
+//     } else if (newPokeType.toLowerCase() === 'electric') {
+//         return 'fbd200' //rgba(251,210,0,255)
+//     } else {
+//         return 'black'
+//     }
     
-}
-//assign logo
-function assignLogo(newPokeType){
-    if (newPokeType.toLowerCase() === "dark") {
-         return 'Dark.png'
-    } else if (newPokeType.toLowerCase() === 'water'){
-        return 'water.png'
-    } else if (newPokeType.toLowerCase() === 'fire') {
-        return 'fire.png'
-    } else if (newPokeType.toLowerCase() === 'grass'){
-        return 'grass.png'
-    } else if (newPokeType.toLowerCase() === 'rock') {
-        return 'rock.png'
-    } else if (newPokeType.toLowerCase() === 'normal') {
-        return 'normal.png'
-    } else if (newPokeType.toLowerCase() === 'electric') {
-        return 'electric.png'
-    } else {
-        return 'pokeball.png'
-    }
+// }
+// //assign logo
+// function assignLogo(newPokeType){
+//     if (newPokeType.toLowerCase() === "dark") {
+//          return 'Dark.png'
+//     } else if (newPokeType.toLowerCase() === 'water'){
+//         return 'water.png'
+//     } else if (newPokeType.toLowerCase() === 'fire') {
+//         return 'fire.png'
+//     } else if (newPokeType.toLowerCase() === 'grass'){
+//         return 'grass.png'
+//     } else if (newPokeType.toLowerCase() === 'rock') {
+//         return 'rock.png'
+//     } else if (newPokeType.toLowerCase() === 'normal') {
+//         return 'normal.png'
+//     } else if (newPokeType.toLowerCase() === 'electric') {
+//         return 'electric.png'
+//     } else {
+//         return 'pokeball.png'
+//     }
     
-}
+// }
 
 
